e5f038d79e8faf34210a9e3e475dc1dc2014048b
Dan Leslie <dan@ironoxide.ca>
2012-12-03 21:28:34 -0800

    Layers now acts like a normal module

--- render.lua
+++ render.lua
@@ -4,8 +4,8 @@ function init()
    local clr = config.window_clear_color
    MOAIGfxDevice.setClearColor(1, 1, 1, 1)
 
-   for idx,layer in ipairs(layers:getLayers()) do
+   for idx,layer in ipairs(layers.getLayers()) do
       log.info("render", "Pushing layer " .. idx)
-      MOAIRenderMgr:pushRenderPass(layers:get(idx))
+      MOAIRenderMgr:pushRenderPass(layers.get(idx))
    end
 end
